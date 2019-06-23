963f1aa52e504f0b419ec1ac119319a5ac628416
Kaushik Bokka <kaushikbokka@gmail.com>
2019-06-23 15:11:38 +0530

    Add Flask React boilerplate

---
+++ static/react/precache-manifest.fd63337c84f846998978b2e687c92857.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba1416ea932983a15d1ca5c86abee69",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "34d7ff360b50cf215905",
+    "url": "/static/react/css/main.0dfc48c9.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/react/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "34d7ff360b50cf215905",
+    "url": "/static/react/js/main.73da4f49.chunk.js"
+  },
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/js/runtime~main.5d7886d3.js"
+  }
+]);
